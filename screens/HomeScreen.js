22497f9a6f0aa4c3f984cfeb43f6a491752f57c2
LeviPhantom <dinhphuc144@gmail.com>
2020-03-10 15:26:55 -0500

    get Userlocation from expo. not hard coded anymore

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 class HomeScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            region: null
+        }
+        this._getLocationAsync();
+    }
+    _getLocationAsync = async () => {
+        let {status} = await Permissions.askAsync(Permissions.LOCATION);
+        if(status!=='granted')
+            console.log('Permission denied')
+        
+        let location = await Location.getCurrentPositionAsync({enabledHighAccuracy: true})
+        let region = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.045,
+        }
+        this.setState({region:region})
+        console.log(this.state.region)
+
+    }
     render() {
         return (
             <View style={styles.container}>
                 <MapView
-              provider={PROVIDER_GOOGLE}
               style={{flex:1}}
-              region={{
-                  latitude: 42.88204,
-                  longitude: 74.582748,
-                  latitudeDelta:0.0922,
-                  longitudeDelta:0.0421
-              }}
-              showsUserLocation
+              region={this.state.region}
+              showsUserLocation={true}
           />
             </View>
        
