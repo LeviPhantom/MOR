84761701671c1ccc59d1c3b647623d2ab925eee2
Kristina Carrone <kristina.carrone@selu.edu>
2020-04-07 15:49:26 -0500

    Added Contact info
    All thats been given for contact info of state

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -5,7 +5,19 @@ class NotificationScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Contacts</Text>
+            <Text>Contacts </Text>
+            <Text></Text>
+            <Text>City Limit Contacts:</Text>
+            <Text>Abita Springs 985-892-0711</Text>
+            <Text>Covington 985-898-4700</Text>
+            <Text>Mandeville 985-624-3169 </Text>
+            <Text>Pearl River 985-863-5800</Text>
+            <Text> Slidell 985-646-4258  </Text>
+            <Text></Text>
+            <Text>For all reports outside city limits:</Text>
+            <Text>Please contact St. Tammany Parish or State Transportation</Text>
+            
+            
       </View>
     );
   }
