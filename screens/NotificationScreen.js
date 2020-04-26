f937fa3eebc7e88c17b0cfb14a9e19f96787522e
LeviPhantom <dinhphuc144@gmail.com>
2020-04-25 19:29:35 -0500

    Rendering post

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -1,28 +1,63 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import * as firebase from "firebase";
-
+import { Ionicons } from "@expo/vector-icons";
 class NotificationScreen extends Component {
-  state = {
-    posts: null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      isLoading: false,
+    };
+  }
+  renderPost = (post) => {
+    return (
+      <View style={styles.feedItem}>
+        <View style={{ flex: 1 }}>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <Text style={styles.name}>{post.email}</Text>
+              <Text style={styles.name}>{post.address}</Text>
+              <Text style={styles.timestamp}>{post.timestamp}</Text>
+            </View>
+            <Ionicons name="ios-more" size={24} color="#73788B" />
+          </View>
+          <Text style={styles.posts}>{post.description}</Text>
+          <Image
+            source={{ uri: post.image }}
+            style={styles.postImage}
+            resizeMethod="auto"
+          />
+        </View>
+      </View>
+    );
   };
-  // renderPost = post => {
-  //     return(
-  //         <View>
-  //             <Text>I'm a post</Text>
-  //         </View>
-  //     );
-  // }
-  componentDidMount() {
+  getPosts() {
+    this.setState({ isLoading: true });
     const firestore = firebase.firestore();
+    const x = [];
     firestore
       .collection("post")
       .get()
       .then((snapshot) => {
         snapshot.docs.forEach((doc) => {
-          this.setState({ posts: [doc.data()] });
+          x.push(doc.data());
+          //console.log(doc.data());
         });
+
+        this.setState({ posts: x });
       });
+    this.setState({ isLoading: false });
+    console.log(this.state.posts);
+  }
+  componentDidMount() {
+    this.getPosts();
   }
   render() {
     return (
@@ -30,6 +65,13 @@ class NotificationScreen extends Component {
         <View style={styles.header}>
           <Text style={styles.headerTitle}>Feed</Text>
         </View>
+        <FlatList
+          style={styles.feed}
+          data={this.state.posts}
+          renderItem={({ item }) => this.renderPost(item)}
+          keyExtractor={(item) => item.image}
+          showsVerticalScrollIndicator={false}
+        />
       </View>
     );
   }
@@ -37,6 +79,7 @@ class NotificationScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#EFECF4",
   },
   header: {
     color: "#D8D9DD",
@@ -52,6 +95,41 @@ const styles = StyleSheet.create({
     shadowRadius: 15,
     shadowOpacity: 0.2,
     zIndex: 10,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  feed: {
+    marginHorizontal: 16,
+  },
+  feedItem: {
+    backgroundColor: "#FFF",
+    borderRadius: 5,
+    padding: 8,
+    flexDirection: "row",
+    marginVertical: 8,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "#454D65",
+  },
+  timestamp: {
+    fontSize: 11,
+    color: "#C4C6CE",
+    marginTop: 4,
+  },
+  post: {
+    marginTop: 16,
+    fontSize: 14,
+    color: "#838899",
+  },
+  postImage: {
+    width: undefined,
+    height: 150,
+    borderRadius: 5,
+    marginVertical: 16,
   },
 });
 export default NotificationScreen;
