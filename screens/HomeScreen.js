a5f8d2b82417e9e683fc70767e35d4b72914cffd
LeviPhantom <dinhphuc144@gmail.com>
2020-04-26 23:00:07 -0500

    Displaying Image on map

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
 import * as firebase from "firebase";
+const { width, height } = Dimensions.get("screen");
 
 class HomeScreen extends Component {
   constructor(props) {
@@ -13,11 +14,17 @@ class HomeScreen extends Component {
     this.state = {
       region: null,
       markers: [],
+      destination: "../assets/icon.png",
     };
+    this._isMounted = false;
     this._getLocationAsync();
   }
   componentDidMount() {
+    this._isMounted = true;
     this.getMapMarkers();
+  }
+  componentWillUnmount() {
+    this._isMounted = false;
   }
   getMapMarkers() {
     const firestore = firebase.firestore();
@@ -29,10 +36,15 @@ class HomeScreen extends Component {
         snapshot.docs.forEach((doc) => {
           array.push(doc.data());
         });
-        this.setState({ markers: array });
+        this._isMounted && this.setState({ markers: array });
       });
   }
-
+  onMarkerPress = (location) => () => {
+    this.setState({
+      destination: location.image,
+    });
+    console.log(this.state.destination);
+  };
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status !== "granted") console.log("Permission denied");
@@ -46,10 +58,11 @@ class HomeScreen extends Component {
       latitudeDelta: 0.045,
       longitudeDelta: 0.045,
     };
-    this.setState({ region: region });
+    this._isMounted && this.setState({ region: region });
   };
 
   render() {
+    const { destination } = this.state;
     if (this.state.markers !== null) {
       return (
         <View style={styles.container}>
@@ -67,8 +80,20 @@ class HomeScreen extends Component {
                   latitude: marker.latitude,
                   longitude: marker.longitude,
                 }}
-              />
+                onPress={this.onMarkerPress(marker)}
+              ></MapView.Marker>
             ))}
+            <Image
+              source={{ uri: this.state.destination }}
+              style={{
+                flex: 1,
+                width: width * 0.95,
+                alignSelf: "center",
+                height: height * 0.15,
+                position: "absolute",
+                bottom: height * 0.05,
+              }}
+            />
           </MapView>
         </View>
       );
